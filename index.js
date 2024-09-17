feb8cf5d151eb4f6a2829b2561f59d363bf29918
hamzaali01 <82397654+hamzaali01@users.noreply.github.com>
2024-09-18 03:13:16 +0500

    Updates index.js for adding access control allow origin

--- index.js
+++ index.js
@@ -10,18 +10,24 @@ const placesRoutes = require("./routes/places-routes");
 const HttpError = require("./models/http-error");
 const cors = require("cors");
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 const app = express();
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization "
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+    next();
+});
 
 // Routes
 app.use("/api/places", placesRoutes);
